7f3c89c486d897eef0d4839990c661ebc6b82ba2
fabezio <fabezio@outlook.fr>
2021-02-20 19:18:34 +0100

    affichage première page

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react'
+
+const App: FunctionComponent = () => {
+  const name: String = 'React'
+
+  return <h1>Salut, {name} ! </h1>
+}
+export default App
