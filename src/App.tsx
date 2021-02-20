eeb15e21ccb83b2f86e113e16efe9c199e4daa93
fabezio <fabezio@outlook.fr>
2021-02-20 20:06:54 +0100

    Ajout pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useState } from 'react'
+import POKEMONS from './models/mock-pokemon.ts'
+import Pokemon from './models/pokemon.ts'
 
 const App: FunctionComponent = () => {
-  const name: String = 'React'
+  const [pokemons] = useState<Pokemon[]>(POKEMONS)
+  //   const [name, setName] = useState<String>('React 16,')
 
-  return <h1>Salut, {name} ! </h1>
+  return <h1>Vous avez {POKEMONS.length} pokemons !</h1>
+  //   return <h1>Salut, {name} ! </h1>
 }
 export default App
