f684902653722f074d708e221798282b065187c5
fabezio <fabezio@outlook.fr>
2021-02-21 05:56:27 +0100

    affichage pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import React, { FunctionComponent, useState } from 'react'
-import POKEMONS from './models/mock-pokemon.ts'
-import Pokemon from './models/pokemon.ts'
+import React, { FunctionComponent, useState, useEffect } from 'react'
+import POKEMONS from './models/mock-pokemon'
+import Pokemon from './models/pokemon'
 
 const App: FunctionComponent = () => {
-  const [pokemons] = useState<Pokemon[]>(POKEMONS)
+  const [pokemons, setPokemons] = useState<Pokemon[]>([])
+  // pokemons.length=0
+  for (let pika of pokemons  ) {
+    console.log(pika.name)
+  }
+  // const [pokemons] = useState<Pokemon[]>(POKEMONS)
   //   const [name, setName] = useState<String>('React 16,')
 
-  return <h1>Vous avez {POKEMONS.length} pokemons !</h1>
+  useEffect(() => {
+    setPokemons(POKEMONS)
+
+  }, [])
+  return (
+    <div>
+      <h1>Contenu du PokeDex</h1>
+      {/* <h2>{pokemons.length}</h2> */}
+
+      <p>{pokemons.map(({ name }) => {
+        <span key={name}>{name}
+        </span>
+      })}</p>
+
+
+    </div>
+  )
   //   return <h1>Salut, {name} ! </h1>
 }
 export default App
